eec98e31fe878242de70b609100d6a136b9d01f2
AMoret01 <57366094+AMoret01@users.noreply.github.com>
2023-03-22 20:58:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,30 +13,47 @@ def generate_image():
 
     # Obtener la descripción de la imagen desde el cuerpo de la solicitud
     prompt = request.json['prompt']
-   
-    # Define la URL de la API y los datos de entrada
-    url = 'https://api.openai.com/v1/images/generations'
-    data = {
-        'prompt': prompt,
-        'n': 2,
-        'size': '512x512',
-        'response_format': 'url',
-        'user': 'my-unique-user-id'
-    }
+    
+    completion = openai.Completion.create(  engine="text-davinci-003",
+                                            prompt=f"clasifica el siguiente texto con solo una de las siguientes opciones: vulgar,discriminatorio,violento, texto aceptable: {prompt}",
+                                            max_tokens=2048)
+                        
+    response = completion.choices[0].text
+    
+    if 'vulgar' in response.lower():
+        response=1
+    elif 'discriminatorio' in response.lower():
+        response=2
+    elif 'violento' in response.lower():
+        response=3
+    else:
+        response=4
+   if (response<4):
+        return "Su petición contiene contenido inapropiado."
+   else:
+        # Define la URL de la API y los datos de entrada
+        url = 'https://api.openai.com/v1/images/generations'
+        data = {
+            'prompt': prompt,
+            'n': 2,
+            'size': '512x512',
+            'response_format': 'url',
+            'user': 'my-unique-user-id'
+        }
 
-    # Define el encabezado de autorización que incluye su clave API
-    headers = {
-        'Authorization': f'Bearer {api_key}',
-    }
+        # Define el encabezado de autorización que incluye su clave API
+        headers = {
+            'Authorization': f'Bearer {api_key}',
+        }
 
-    # Envía la solicitud HTTP POST con el encabezado de autorización y los datos de entrada
-    response = requests.post(url, json=data, headers=headers)
+        # Envía la solicitud HTTP POST con el encabezado de autorización y los datos de entrada
+        response = requests.post(url, json=data, headers=headers)
 
-    # Obtener la URL de la imagen generada desde la respuesta de la API
-    image_url = response.json()['data']
+        # Obtener la URL de la imagen generada desde la respuesta de la API
+        image_url = response.json()['data']
 
-    # Mostrar la URL de la imagen en la plantilla
-    return jsonify(image_url)
+        # Mostrar la URL de la imagen en la plantilla
+        return jsonify(image_url)
 
 @app.route('/bad-language', methods=['POST'])
 def detect_bad_language():
