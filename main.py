d3fc9d9682eda9353f66dddf705d566ac864a225
Luis Fernando Martínez Gonzalez <114769674+luisfMartinez1303@users.noreply.github.com>
2023-03-24 18:26:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,44 +10,80 @@ import psycopg2
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ["https://frontend-nomad-list.vercel.app", "https://frontend-nomad-list.vercel.app/"]}})
+# Carga la variable de entorno 
+api_key = os.environ.get('OPENAI_API_KEY')
 
 @app.route('/image', methods=['POST'])
 def generate_image():
     
-    # Carga la variable de entorno 
-    api_key = os.environ.get('OPENAI_API_KEY')
+    #Validar mensaje
+
+    # Carga las variables de entorno
+    openai.api_key = api_key
+
+    answers = []
+    # Obtener el texto para verificar
+    text = request.json['prompt']
+
+    completion = openai.Completion.create(  engine="text-davinci-003",
+                                            prompt=f"teniendo en las vulgaridades españolas, clasifica el siguiente texto con solo una de las siguientes opciones: vulgar,discriminatorio,violento, texto aceptable: {text}",
+                                            n=5,
+                                            max_tokens=2048)
+                        
+    for i  in range(0,4):
+        response = completion.choices[i].text
+
+        if 'vulgar' in response.lower():
+            response=1
+        elif 'discriminatorio' in response.lower():
+            response=2
+        elif 'violento' in response.lower():
+            response=3
+        else:
+            response=4
+        answers.append(response)
+
+    def valor_mas_comun(lista):
+        contador = Counter(lista)
+        valor, frecuencia = contador.most_common(1)[0]
+        return valor
+
+    answers = valor_mas_comun(answers)
     
     # Obtener la descripción de la imagen desde el cuerpo de la solicitud
     prompt = request.json['prompt']
 
-    url = 'https://api.openai.com/v1/images/generations'
-    data = {
-        'prompt': prompt,
-        'n': 2,
-        'size': '1024x1024',
-        'response_format': 'url',
-        'user': 'my-unique-user-id'
-    }
+    if answers!=4:
+        return jsonify({'Error':'El texto tiene lenguaje ofensivo'})
+    else:
+        url = 'https://api.openai.com/v1/images/generations'
+        data = {
+            'prompt': prompt,
+            'n': 2,
+            'size': '1024x1024',
+            'response_format': 'url',
+            'user': 'my-unique-user-id'
+        }
 
-    # Define el encabezado de autorización que incluye su clave API
-    headers = {
-        'Authorization': f'Bearer {api_key}',
-    }
+        # Define el encabezado de autorización que incluye su clave API
+        headers = {
+            'Authorization': f'Bearer {api_key}',
+        }
 
-    # Envía la solicitud HTTP POST con el encabezado de autorización y los datos de entrada
-    response = requests.post(url, json=data, headers=headers)
+        # Envía la solicitud HTTP POST con el encabezado de autorización y los datos de entrada
+        response = requests.post(url, json=data, headers=headers)
+        
+        # Obtener la URL de la imagen generada desde la respuesta de la API
+        image_url = response.json()['data']
 
-    # Obtener la URL de la imagen generada desde la respuesta de la API
-    image_url = response.json()['data']
-
-    # Mostrar la URL de la imagen en la plantilla
-    return jsonify(image_url)
+        # Mostrar la URL de la imagen en la plantilla
+        return jsonify(image_url)
         
 @app.route('/bad-language', methods=['POST'])
 def detect_bad_language():
 
     # Carga las variables de entorno
-    openai.api_key = os.getenv('OPENAI_API_KEY')
+    openai.api_key = api_key
     
     answers = []
     # Obtener el texto para verificar
@@ -84,7 +120,7 @@ def detect_bad_language():
 def detect_snetiment():
 
     # Carga las variables de entorno
-    openai.api_key = os.getenv('OPENAI_API_KEY')
+    openai.api_key = api_key
 
     # Obtener el texto para verificar
     text = request.json['text']
